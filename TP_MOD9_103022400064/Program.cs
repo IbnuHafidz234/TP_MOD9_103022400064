253c5e08391ff97c53e7aeeff43efea3d136194f
nu <hafidzibnu035@gmail.com>
2026-04-25 09:44:26 +0700

    Menambahkan Implementasi Runtime Configuration

--- TP_MOD9_103022400064/Program.cs
+++ TP_MOD9_103022400064/Program.cs
@@ -1,1 +1,52 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using TP_MOD9_103022400064;
+
+namespace TP_MOD9_103022400064
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CovidConfig covidConfig = new CovidConfig();
+
+            // Jalankan pertama kali dengan satuan default
+            JalankanAplikasi(covidConfig);
+
+            Console.WriteLine("\n--- Mengubah Satuan Suhu ---");
+            covidConfig.UbahSatuan();
+            JalankanAplikasi(covidConfig);
+        }
+
+        static void JalankanAplikasi(CovidConfig configObj)
+        {
+            Config currentConf = configObj.conf;
+
+            Console.Write($"Berapa suhu badan anda saat ini? Dalam nilai {currentConf.satuan_suhu}: ");
+            double suhu = double.Parse(Console.ReadLine());
+
+            Console.Write("Berapa hari yang lalu (perkiraan) anda terakhir memiliki gejala deman? ");
+            int hari = int.Parse(Console.ReadLine());
+
+            bool suhuValid = false;
+            if (currentConf.satuan_suhu == "celcius")
+            {
+                if (suhu >= 36.5 && suhu <= 37.5) suhuValid = true;
+            }
+            else if (currentConf.satuan_suhu == "fahrenheit")
+            {
+                if (suhu >= 97.7 && suhu <= 99.5) suhuValid = true;
+            }
+
+            bool hariValid = hari < currentConf.batas_hari_deman;
+
+            if (suhuValid && hariValid)
+            {
+                Console.WriteLine(currentConf.pesan_diterima);
+            }
+            else
+            {
+                Console.WriteLine(currentConf.pesan_ditolak);
+            }
+        }
+    }
+}
--- TP_MOD9_103022400064/obj/Debug/net10.0/TP_MOD9_103022400064.AssemblyInfo.cs
+++ TP_MOD9_103022400064/obj/Debug/net10.0/TP_MOD9_103022400064.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP_MOD9_103022400064")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3ee580f0c6259609938b30251fc8c6df08465dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP_MOD9_103022400064")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP_MOD9_103022400064")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
